f11c9e00f4d7e5ae843feba66f959e016c78cd4e
marvin255 <postbox433@gmail.com>
2023-08-18 15:31:22 +0200

    Fix SourceReaderArrayTest

--- tests/SourceReaderArrayTest.php
+++ tests/SourceReaderArrayTest.php
@@ -31,6 +31,7 @@ final class SourceReaderArrayTest extends BaseCase
     {
         $options = [
             'test' => 'test',
+            'test1' => 'test 1',
         ];
 
         $reader = new SourceReaderArray();
